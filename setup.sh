bc5ab433de883eb45832716982da95c53a267f3c
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-10-30 06:27:07 +0330

    feat: use config files from personal dotfile repo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# ================= activate sudo mode =================
+if [ "$EUID" != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+
+# ======================= welcome =======================
 echo
 echo "wassssuuuuup....!"
 echo "script will install basic tools and setupup configs for the new user."
@@ -13,13 +21,15 @@ if [[ $REPLY =~ ^[Nn]$ ]]; then
 fi
 
 
-# install stuff
+# ==================== install stuff ====================
 echo
 echo "Installing packages..."
 echo
 apt install -y git tmux nodejs npm fonts-powerline
 npm -g install pnpm
 
+
+# ========================= zsh =========================
 echo
 echo "Conjuring Zsh magic..."
 echo
@@ -28,24 +38,36 @@ chsh -s $(which zsh) # set zsh as default shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # OhMyZsh
 
 
-# # config stuff
-# echo
-# echo "Configuring stuff..."
-# echo
-# cat .zshrc >> ~/.zshrc
+# ================ download config files ================
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# cp .tmux.conf ~/.tmux.conf
-# cp .nanorc ~/.nanorc
-
-# git config --global user.name "Ryan Norooz"
-# git config --global user.email "58340667+RyanNorooz@users.noreply.github.com"
-# git config --global user.username "RyanNorooz"
-# git config --global core.editor "nano"
+git clone https://github.com/RyanNorooz/dotfiles ~/.dotfiles.tmp
 
 
+# ===================== config stuff =====================
 echo
-echo "Done!"
+echo "Configuring stuff..."
+echo
+cp ~/.dotfiles.tmp/.tmux.conf ~/.tmux.conf
+cp ~/.dotfiles.tmp/.nanorc ~/.nanorc
+cp ~/.dotfiles.tmp/.gitconfig ~/.gitconfig
+
+cat .zshrc >> ~/.zshrc # append to zshrc
+
+
+# ======================= cleanup =======================
+echo
+echo "Cleaning up..."
+echo
+rm -r ~/.dotfiles.tmp
+apt autoremove -y
+apt autoclean -y
+
+
+# ======================= goodbye =======================
+echo
+echo "=================== Done! ==================="
 echo "its a good idea to restart your system now..."
+echo "============================================="
 echo
 
 exit 0
